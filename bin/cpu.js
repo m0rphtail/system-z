3b59d775130a0fe8262d3a7fec0e2136bbd924a0
MorphTail <chitniskshitij@gmail.com>
2021-05-03 14:06:14 +0530

    team red, team blue

--- bin/cpu.js
+++ bin/cpu.js
@@ -1,10 +1,25 @@
 const os = require("os");
+const chalk = require("chalk");
 
 module.exports = function () {
   const cpu = os.cpus();
-  const model = cpu[0].model;
+  const fullName = cpu[0].model;
   const speed = cpu[0].speed;
 
-  console.log(`cpu: ${model}`);
-  console.log(`clock speed: ${speed} MHz`);
+  for (var i = 0; i < fullName.length; i++) {
+    var maker = fullName.substr(0, fullName.indexOf(" "));
+  }
+
+  for (var i = 0; i < fullName.length; i++) {
+    var model = fullName.substr(maker.length + 1, fullName.length);
+  }
+
+  if (maker == "AMD") {
+    console.log("CPU         :", chalk.red(maker), model);
+  } else if (maker == "INTEL") {
+    console.log("CPU         :", chalk.blue(maker), model);
+  } else {
+    console.log(`CPU         : ${maker} ${model}`);
+  }
+  console.log(`Clock Speed : ${speed} MHz`);
 };
