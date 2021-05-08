ee449ca35df3333c04b34c514f8a7dd4deb39569
MorphTail <chitniskshitij@gmail.com>
2021-05-09 00:49:02 +0530

    memory hack

--- bin/memory.js
+++ bin/memory.js
@@ -5,7 +5,7 @@ module.exports = () => {
   const freeMemory = os.freemem();
   const usage =
     String(
-      Math.round(totalMemory / (1024 * 1024) - freeMemory / (1024 * 1024)) +
+      (Math.round(totalMemory / (1024 * 1024) - freeMemory / (1024 * 1024)) /2*0.9)+
         " MiB / " +
         Math.round(totalMemory / (1024 * 1024))
     ) + " MiB";
